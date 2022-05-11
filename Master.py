5f1d24ab4294aebeeeb8f7cda0bb3150a7af70f5
parmarparth0111 <parmarparth0111@gmail.com>
2022-05-11 13:46:46 +0530

     

--- Master.py
+++ Master.py
@@ -7,12 +7,13 @@ import os
 s = socket.socket()
 host socket.gethostname()
 port = 8880
-s.bind(", port)
+s.bind('', port)
 s.Listen()
 conn, addr = s.accept()
 print.(addr, "Ls connected to server")
 command = Input (str("Enter Command :"))
 conn.send(command.encode())
 print("Command has been sent successfully.")
-data = conn.recv(1024) 16 Lr data:
-print("command received and executed successfully.")
+data = conn.recv(1024)
+if data:
+    print("command received and executed successfully.")
