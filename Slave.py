0c06186a6ee0842190cc03282b840955d5f448b8
parmarparth0111 <parmarparth0111@gmail.com>
2022-05-11 13:47:52 +0530

     

--- Slave.py
+++ Slave.py
@@ -4,14 +4,14 @@ import time
 import socket
 import sys
 import os
-S = socket.socket()
+s = socket.socket()
 host = "127.0.0.1"
 port = 8080
 s.connect((host, port))
 print("Connected to Server.")
 command = s.recv(1024)
 command = command.decode()
-Lf command == "open":
-print("Command is ", command)
-s.send("Command received". encode())
-os.system('ls')
+if command == "open":
+    print("Command is ", command)
+    s.send("Command received". encode())
+    os.system('ls')
