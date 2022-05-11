fce8f7a7eeb31ce9d060dfba2a860313215ccfd0
parmarparth0111 <parmarparth0111@gmail.com>
2022-05-11 13:29:11 +0530

    hello

---
+++ Master.py
@@ -0,0 +1,18 @@
+# Master Computer
+
+import time
+import socket
+import sys
+import os
+s = socket.socket()
+host socket.gethostname()
+port = 8880
+s.bind(", port)
+s.Listen()
+conn, addr = s.accept()
+print.(addr, "Ls connected to server")
+command = Input (str("Enter Command :"))
+conn.send(command.encode())
+print("Command has been sent successfully.")
+data = conn.recv(1024) 16 Lr data:
+print("command received and executed successfully.")
---
+++ Slave.py
@@ -0,0 +1,17 @@
+# Slave Computer
+
+import time
+import socket
+import sys
+import os
+S = socket.socket()
+host "127.0.0.1" =
+port = 8080
+s.connect((host, port))
+print("Connected to Server.")
+command = s.recv(1024)
+command = command.decode()
+Lf command == "open":
+print("Command is ", command)
+s.send("Command received". encode())
+os.system('ls')
